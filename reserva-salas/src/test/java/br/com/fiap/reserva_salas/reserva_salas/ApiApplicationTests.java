922a41421421f3eaf4f5027242dd01e81f623a0a
Karyah <mastrogiacomokaren@gmail.com>
2024-10-24 15:29:49 -0300

    Importando projeto do Spring Initialzr

---
+++ reserva-salas/src/test/java/br/com/fiap/reserva_salas/reserva_salas/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.reserva_salas.reserva_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
