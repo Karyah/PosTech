a7f8dc7f818032587d8e7c8dee5a3eaf43097dcf
Karyah <mastrogiacomokaren@gmail.com>
2024-10-25 15:40:46 -0300

    Criando mais endpoints para Usuario

--- reserva-salas/src/main/java/br/com/fiap/reserva_salas/controller/UsuarioController.java
+++ reserva-salas/src/main/java/br/com/fiap/reserva_salas/controller/UsuarioController.java
@@ -1,13 +1,18 @@
 package br.com.fiap.reserva_salas.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +38,17 @@ public class UsuarioController {
 	public ResponseEntity<UsuarioDTO> save(@Valid @RequestBody UsuarioDTO usuarioDTO) {
 		UsuarioDTO usuarioSalvo = service.save(usuarioDTO);
 		return new ResponseEntity<>(usuarioSalvo, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<UsuarioDTO> update(@PathVariable UUID id, @Valid @RequestBody UsuarioDTO usuarioDTO){
+		UsuarioDTO buscaUsuarioDTO = service.update(id, usuarioDTO);
+		return ResponseEntity.ok(buscaUsuarioDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable UUID id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- reserva-salas/src/main/java/br/com/fiap/reserva_salas/service/UsuarioService.java
+++ reserva-salas/src/main/java/br/com/fiap/reserva_salas/service/UsuarioService.java
@@ -1,12 +1,17 @@
 package br.com.fiap.reserva_salas.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import br.com.fiap.reserva_salas.controller.exception.ControllerNotFoundException;
 import br.com.fiap.reserva_salas.dto.UsuarioDTO;
 import br.com.fiap.reserva_salas.entity.Usuario;
 import br.com.fiap.reserva_salas.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -21,14 +26,37 @@ public class UsuarioService {
 	}
 	
 	public UsuarioDTO save(UsuarioDTO usuarioDTO) {
-		Usuario usuario = new Usuario(usuarioDTO.id(), usuarioDTO.nome(), usuarioDTO.cpf(), usuarioDTO.email());
+		Usuario usuario = toUsuario(usuarioDTO);
 		repository.save(usuario);
 		return toDTO(usuario);
+	}
+	
+	public UsuarioDTO update(UUID id, UsuarioDTO usuarioDTO) {
+		try {
+			Usuario buscaUsuario = repository.getReferenceById(id);
+			buscaUsuario.setNome(usuarioDTO.nome());
+			buscaUsuario.setCpf(usuarioDTO.cpf());
+			buscaUsuario.setEmail(usuarioDTO.email());
+			buscaUsuario = repository.save(buscaUsuario);
+			return toDTO(buscaUsuario);
+		}catch(EntityNotFoundException e) {
+			throw new ControllerNotFoundException("Usuário não encontrado");
+		}
+		
+	}
+	
+	public void delete(UUID id) {
+		repository.deleteById(id);
 	}
 	
 	public UsuarioDTO toDTO(Usuario usuario) {
 		UsuarioDTO usuarioDTO = new UsuarioDTO(usuario.getId(),usuario.getNome(),usuario.getCpf(), usuario.getEmail()); 
 		return usuarioDTO;
+	}
+	
+	public Usuario toUsuario(UsuarioDTO usuarioDTO) {
+		Usuario usuario = new Usuario(usuarioDTO.id(), usuarioDTO.nome(), usuarioDTO.cpf(), usuarioDTO.email());
+		return usuario;
 	}
 	
 }
